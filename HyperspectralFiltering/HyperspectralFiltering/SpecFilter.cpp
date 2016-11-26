d23b023d1b908de7e81f7c018b5dbe3279f883f9
Anthony 'Bitzawolf' Pepe <bitzah@gmail.com>
2016-11-26 13:43:57 -0800

    almost finished. Haha, whoops! Need to commit more.

--- HyperspectralFiltering/HyperspectralFiltering/SpecFilter.cpp
+++ HyperspectralFiltering/HyperspectralFiltering/SpecFilter.cpp
@@ -1,22 +1,126 @@
+/*
+	Implements SpecFilter.h
+	@author Anthony Pepe
+*/
 #include "SpecFilter.h"
 
+/*
+Creates a new filter with no values for any wavelength. Users
+can set wavelength-reflectance values directly via SetIntensity(),
+or can upload a USGS reflectance file via LoadFromFile().
+*/
 SpecFilter::SpecFilter()
 {
-	// Create a blank filter
-	filterData.push_back({0, 0});
+	// Nothing to construct
 }
 
-void SpecFilter::LoadFromFile(std::string fileName)
+/*
+Returns the reflectance intensity of the filter at the specified wavelength.
+@param wavelength Wavelength in nanometers. (1,000 nanometers = 1 micrometer)
+@return Reflectance intensity of the filter at that wavelength.
+*/
+double SpecFilter::GetIntensityNano(const int& wavelength) const
 {
-
+	double wavelengthMicro = wavelength / 1000.0;
+	return GetIntensityMicro(wavelengthMicro);
 }
 
-int SpecFilter::GetIntensity(int wavelength) const
+/*
+Returns the reflectance intensity of the filter at the specified wavelength.
+@param wavelength Wavelength in micrometers.
+@return Reflectance intensity of the filter at that wavelength.
+*/
+double SpecFilter::GetIntensityMicro(const double& wavelength) const
 {
-	return 0;
+	map<double, double>::const_iterator iter = filterData.find(wavelength);
+	if (iter == filterData.end)
+	{
+		return 0;
+	}
+	return iter->second;
 }
 
-void SpecFilter::SetIntensity(int wavelength, int intensity)
+/*
+Sets the reflectance intensity of the filter at the specified wavelength.
+The intensity must be between 0 and 1.
+@param wavelength Wavelength in nanometers. (1,000 nanometers = 1 micrometer)
+@param intensity Reflectance intensity of the filter. Must be between 0 and 1.
+*/
+void SpecFilter::SetIntensityNano(const int& wavelength, const double& intensity)
 {
+	double wavelengthMicro = wavelength / 1000.0;
+	return SetIntensityMicro(wavelengthMicro, intensity);
+}
 
+/*
+Sets the reflectance intensity of the filter at the specified wavelength.
+The intensity must be between 0 and 1.
+@param wavelength Wavelength in micrometers.
+@param intensity Reflectance intensity of the filter. Must be between 0 and 1.
+*/
+void SpecFilter::SetIntensityMicro(const double& wavelength, const double& intensity)
+{
+	filterData[wavelength] = intensity;
+}
+
+/*
+Loads filter data from a USGS formatted Reflectance Pattern file. Previous filter
+data may be overwritten during this process.
+@param filename Relative file location to load.
+@return True if the file was read successfully, false otherwise.
+@see http://speclab.cr.usgs.gov/spectral.lib06/ds231/datatable.html
+*/
+bool SpecFilter::LoadFromFile(string fileName)
+{
+	ifstream inputFile(fileName);
+	if (!inputFile.is_open())
+	{
+		cerr << "Error - Could not find file \"" << fileName << "\"." << endl;
+		return false;
+	}
+
+	string line;
+	int lineCount = 0;
+	while (!inputFile.eof())
+	{
+		getline(inputFile, line);
+		if (lineCount < 17) // Data starts at line 17
+		{
+			++lineCount;
+			continue;
+		}
+		if (line.find("-1.23e34") == string::npos) // Indicates data does not exist.
+		{
+			continue;
+		}
+
+		stringstream lineIn(line);
+		double wavelength, reflectance;
+		lineIn >> wavelength;
+		lineIn >> reflectance;
+
+		filterData[wavelength] = reflectance;
+	}
+
+	return true;
+}
+
+/*
+Finds pixles in a target image that have similar reflectance values to this filter.
+Returns a greyscale image where black and dark pixels are "poor matches" and bright or
+white pixels are "strong matches."
+@param image Hyperspectral image to run this filter against.
+@return Greyscale image where bright/white pixels correspond to positive results from this
+filter.
+*/
+Mat SpecFilter::filter(const SpecImage& image) const
+{
+	map<double, double>::const_iterator i = filterData.begin();
+	while (i != filterData.end)
+	{
+		int wavelength = static_cast<int>(i->first);
+		double reflectance = i->second;
+		image.getImage(wavelength);
+
+	}
 }
--- HyperspectralFiltering/HyperspectralFiltering/SpecFilter.h
+++ HyperspectralFiltering/HyperspectralFiltering/SpecFilter.h
@@ -1,29 +1,86 @@
 /*
-SpecFilter represents a resonance pattern that spectral images
-can be filtered against to determine where the target objects
-are in a hyperspectral image.
+SpecFilter represents a resonance pattern of a physical object.
+Spectral images can be filtered to determine if (and where) the target objects
+are present, in a hyperspectral image.
+
+The filter stores a map of reflectance values where each wavelength has some normalized
+value of expected reflectance at that wavelength. The Sum of Absolute Differences (SAD)
+technique is used to compare the hyperspectral image against the filter.
+
+@author Anthony Pepe
 */
 #pragma once
+#include <opencv2/core/core.hpp>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
 #include <string>
-#include <vector>
 
+#include "SpecImage.h"
+
+using namespace cv;
 using namespace std;
 
 class SpecFilter
 {
 	public:
+		/*
+		  Creates a new filter with no values for any wavelength. Users
+		  can set wavelength-reflectance values directly via SetIntensity(),
+		  or can upload a USGS reflectance file via LoadFromFile().
+		*/
 		SpecFilter();
 
-		void LoadFromFile(string fileName);
-		int GetIntensity(int wavelength) const; 
-		void SetIntensity(int wavelength, int intensity);
+		/*
+		  Returns the reflectance intensity of the filter at the specified wavelength.
+		  @param wavelength Wavelength in nanometers. (1,000 nanometers = 1 micrometer)
+		  @return Reflectance intensity of the filter at that wavelength.
+		*/
+		double GetIntensityNano(const int& wavelength) const;
+
+		/*
+		  Returns the reflectance intensity of the filter at the specified wavelength.
+		  @param wavelength Wavelength in micrometers.
+		  @return Reflectance intensity of the filter at that wavelength.
+		*/
+		double GetIntensityMicro(const double& wavelength) const;
+
+		/*
+		  Sets the reflectance intensity of the filter at the specified wavelength.
+		  The intensity must be between 0 and 1.
+		  @param wavelength Wavelength in nanometers. (1,000 nanometers = 1 micrometer)
+		  @param intensity Reflectance intensity of the filter. Must be between 0 and 1.
+		*/
+		void SetIntensityNano(const int& wavelength, const double& intensity);
+
+		/*
+		  Sets the reflectance intensity of the filter at the specified wavelength.
+		  The intensity must be between 0 and 1.
+		  @param wavelength Wavelength in micrometers.
+		  @param intensity Reflectance intensity of the filter. Must be between 0 and 1.
+		*/
+		void SetIntensityMicro(const double& wavelength, const double& intensity);
+
+		/*
+		  Loads filter data from a USGS formatted Reflectance Pattern file. Previous filter
+		  data may be overwritten during this process.
+		  @param filename Relative file location to load.
+		  @return True if the file was read successfully, false otherwise.
+		  @see http://speclab.cr.usgs.gov/spectral.lib06/ds231/datatable.html
+		*/
+		bool LoadFromFile(string fileName);
+
+		/*
+		  Finds pixles in a target image that have similar reflectance values to this filter.
+		  Returns a greyscale image where black and dark pixels are "poor matches" and bright or
+		  white pixels are "strong matches."
+		  @param image Hyperspectral image to run this filter against.
+		  @return Greyscale image where bright/white pixels correspond to positive results from this
+		  filter.
+		*/
+		Mat filter(const SpecImage& image) const;
 
 	private:
-		struct filterDataPoint
-		{
-			int wavelength;
-			int intensity;
-		};
-
-		vector<filterDataPoint> filterData;
+		map<double, double> filterData;
 };
